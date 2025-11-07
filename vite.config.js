afd07eefe28a27de08c3fa79c3e22a757144c8a3
DimaManko <mankonat@gmail.com>
2025-11-07 11:45:39 +0300

    dist ceramicsoul

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -10,7 +11,32 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         catalog: resolve(__dirname, "catalog.html"),
+        blog: resolve(__dirname, "blog.html"),
+        about: resolve(__dirname, "about.html"),
       },
     },
   },
+  plugins: [
+    ViteImageOptimizer({
+      include: ["**/*.{jpg,jpeg,png,gif,webp,svg}"],
+      exclude: ["**/node_modules/**", "**/dist/**"],
+
+      // Настройки сжатия
+      jpg: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      png: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80,
+        lossless: false,
+      },
+
+      verbose: true,
+    }),
+  ],
 });
